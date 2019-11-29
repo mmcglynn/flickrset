fcf4fd9cb294965625eba42b9973241f1e310d6d
Michael McGlynn <mike@reignition.net>
2019-11-29 11:30:16 -0500

    Refactoring.

--- js/app.js
+++ js/app.js
@@ -57,141 +57,153 @@ function gallery_title(ajax_data) {
     }
 }
 
-$(document).ready(function () {
-    
-    // Get image
-    var requestImage = function (id, altText, callback, isFirst) {
-        
-        $.ajax({
-            url: api + '?method=flickr.photos.getSizes&api_key=' + api_key + '&photo_id=' + id + '&format=json&jsoncallback=?',
-            type: "GET",
-            cache: true,
-            dataType: 'jsonp',
-            async: true,
-            success: function (data) {
-                
-                var img =  new Image();
-                $(img).attr({
-                    src: data.sizes.size[flickrImageSize].source,
-                    alt: altText
-                });
-                                
-                $('figure').append(img);
-                $('figcaption').text(altText);
-                
-                //console.log(img);
-                            
-                // Only run if this is the first image
-                if (isFirst) {
-                    $(img).load(function () {
-                        console.log('image has loaded');
-                        $(this).addClass('active');
-                        placeArrows();
-                        callback();
-                    });
-                }
-                   
-            },
-            error: function () {
-                console.log('Image was not retrieved.');
-            }
-        });
-    };
 
-    // NEED TO PASS THE PHOTOS ARRAY
-    // THEN YOU CAN CALL AND WRITE THE IMAGE ONE AT A TIME
-    // YOU CAN ALSO RUN A FUNCTION TO JUST TO THE FIRST ONE
-    // PASSING THE WHOLE DATA OBJECT WAS STUPID
-    
-    
-    // Handle the data from the API request above    
-    function handleData(ajax_data) {
-        var photolist = ajax_data.photoset.photo,
-            photo_total = photolist.length,
-            next = 1,
-            i = 0,
-            j = 0;
-        
-        for (i = 0; i < photo_total; i = i + 1) {
-            // The list of photo ids to look up
-            flickr_id_array.push(photolist[i].id);
-            // The list of photo captions
-            flickr_title_array.push(photolist[i].title);
-        }
-        
-        // Write the initial caption
-        $('figcaption').text(flickr_title_array[0]);
-        
-        //for (j = 0; j < photo_total; j = j + 1) {
-            //requestImage(flickr_id_array[j], flickrImageSize);            
-        //}
-    
-        
-        $('#next').on('click', function (e) {
-            console.log('next clicked');
-            
-            e.preventDefault();
+var requestImageSimple = function (id, altText) {
 
-            // Determine the next image on the list
-            var next_index = ($('img.active').index());
-            
-            console.log(next_index);
-            
-            // Remove all class instances
-            $('img').removeClass('active');
-            
-            // If the active image is the last in the sequence
-            if (next_index > photo_total) { next_index = 0; }
-            
-            // Activate the next image
-            $('img:eq(' + next_index + ')').addClass('active');
-            
-            // Write the caption
-            // next = next + 1;
-            //$('figcaption').text(flickr_title_array[next]);
-            // Write the caption
-            //$('figcaption').text(flickr_title_array[next_index]);
-            
-        });
-        
-        $('#prev').on('click', function (e) {
-            e.preventDefault();
-            
-            if ($('img').length === 1) { return; }
-            
-            var prev_index = ($('img.active').index()) - 1;
-            
-            // Using 3 for testing only
-            if (prev_index === -1) { prev_index = (photo_total - 1); }
-            
-            console.log('prev_index: ' + prev_index);
-            
-            // Write the caption
-            $('figcaption').text(flickr_title_array[prev_index]);
-            
-            // Move the class backward
-            $('img').removeClass('active');
-            $('img:eq(' + prev_index + ')').addClass('active');
+    $.ajax({
+        url: api + '?method=flickr.photos.getSizes&api_key=' + api_key + '&photo_id=' + id + '&format=json&jsoncallback=?',
+        type: "GET",
+        cache: true,
+        dataType: 'jsonp',
+        async: true,
+        success: function (data) {
 
-        });
-
-    }
-
-    function handlePhotosArray(photos) {
-        
-        //photos.shift();
-        
-        //console.log(photos[0][0]);
-        
-        requestImage(photos[0][0], photos[0][1], function () {
-            console.log('callback called');
-            $.each(photos, function (i, obj) {
-                //console.log(obj[0] + ', ' + obj[1]);
-                requestImage(obj[0], obj[1], false);
+            // Create a new image object
+            var img =  new Image();
+            $(img).attr({
+                src: data.sizes.size[flickrImageSize].source,
+                alt: altText
             });
-        }, true);
-        
-    }
+
+            // Add these to the UI
+            $('figure').append(img);
+
+        },
+        error: function () {
+            console.log('Image was not retrieved.');
+        }
+    });
+};
+
+
+// Get the individual image
+// This is the current problem in that images
+// are not being downloaded sequntially.
+// please test as async = false
+var requestImage = function (id, altText, callback, isFirst) {
+
+    $.ajax({
+        url: api + '?method=flickr.photos.getSizes&api_key=' + api_key + '&photo_id=' + id + '&format=json&jsoncallback=?',
+        type: "GET",
+        cache: true,
+        dataType: 'jsonp',
+        async: true,
+        success: function (data) {
+
+            // Create a new image object
+            var img =  new Image();
+            $(img).attr({
+                src: data.sizes.size[flickrImageSize].source,
+                alt: altText
+            });
+
+            // Add these to the UI
+            $('figure').append(img);
+            $('figcaption').text(altText);
+
+            //console.log(img);
+
+            // If is the first image, run the callback function 
+            // that loads the rest of the images
+            if (isFirst) {
+                $(img).load(function () {
+                    console.log('image has loaded');
+                    $(this).addClass('active');
+                    placeArrows();
+                    callback();
+                });
+            }
+
+        },
+        error: function () {
+            console.log('Image was not retrieved.');
+        }
+    });
+};
+
+// Once the image data has been determined,
+// requested the correct resolution from the server
+function handlePhotosArray(photos) {
+
+    // id, altText, callback, isFirst
+    requestImage(photos[0][0], photos[0][1], function () {
+        console.log('callback called');
+        $.each(photos, function (i, obj) {
+            console.log(obj[0] + ', ' + obj[1]);
+            
+            setTimeout(function(){ 
+                requestImage(obj[0], obj[1], false);
+            }, 6000);
+            
+            
+        });
+    }, true);
+
+}
+
+function navigation(total) {
+
+    $('#next').on('click', function (e) {
+
+    e.preventDefault();
+
+    // Determine the next image on the list
+    var next_index = ($('img.active').index());
+
+    console.log(next_index);
+
+    // Remove all class instances
+    $('img').removeClass('active');
+
+    // If the active image is the last in the sequence
+    if (next_index > total) { next_index = 0; }
+
+    // Activate the next image
+    $('img:eq(' + next_index + ')').addClass('active');
+
+    // Write the caption
+    // next = next + 1;
+    //$('figcaption').text(flickr_title_array[next]);
+    // Write the caption
+    //$('figcaption').text(flickr_title_array[next_index]);
+
+    });
+    
+    
+    $('#prev').on('click', function (e) {
+        e.preventDefault();
+
+        if ($('img').length === 1) { return; }
+
+        var prev_index = ($('img.active').index()) - 1;
+
+        // Using 3 for testing only
+        if (prev_index === -1) { prev_index = (photo_total - 1); }
+
+        console.log('prev_index: ' + prev_index);
+
+        // Write the caption
+        $('figcaption').text(flickr_title_array[prev_index]);
+
+        // Move the class backward
+        $('img').removeClass('active');
+        $('img:eq(' + prev_index + ')').addClass('active');
+
+    });
+    
+}
+
+$(document).ready(function () {
     
     // Get gallery title and photos
     // Why jsonp?
@@ -211,6 +223,10 @@ $(document).ready(function () {
                 //console.log(obj.title);
             });
             
+            console.log(photosArray.length);
+            
+            navigation(photosArray.length);
+            
             handlePhotosArray(photosArray);
         },
         error: function () {
@@ -219,3 +235,6 @@ $(document).ready(function () {
     });
     
 });
+
+
+$(window).on('load', function () {});
