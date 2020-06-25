014c788c9da36e535984dfa9f7085d7b293b9bc8
Michael McGlynn <mike@reignition.net>
2020-06-25 07:04:36 -0400

    Continued refactoring of the navigation.

--- js/app.js
+++ js/app.js
@@ -4,7 +4,8 @@
 // jsoncallback=?
 
 // TO DO
-// - Make pervious button appear earlier, maybe when a 1/4 of the images have loaded?
+// Make pervious button appear earlier, maybe when a 1/4 of the images have loaded?
+// Add keypress support
 
 (function () {
     "use strict";
@@ -36,10 +37,10 @@
     // Place arrows
     placeArrows(window.innerHeight);
     // Adjust placement on window resize
-    window.addEventListener('resize', () => { placeArrows(window.innerHeight) });
+    window.addEventListener('resize', () => { placeArrows(window.innerHeight); });
 
-    // Run when DOM is ready
-    $(document).ready(function () {
+    // Run requestGalleryMembers when DOM is ready
+    jQuery(document).ready(function () {
 
         requestGalleryMembers(function(output){
 
@@ -47,17 +48,20 @@
             let last_index_value = output.length;
 
             // Loop through the created gallery photo array
-            $.each(output, function (index, item) {
+            jQuery.each(output, function (index, item) {
                 // Add the slide containers, which in this case are figures and figcaptions
                 addFigures(item[0],item[1],index + 1);
             });
 
             // Because all figures are initally set to inactive, set the first one to active
-            $("figure:first").removeClass("inactive").addClass("active");
+            jQuery("figure:first").removeClass("inactive").addClass("active");
 
             // Populate the first two images
-            requestImage(output[0][0],output[0][1]);
-            requestImage(output[1][0],output[1][1]);
+            //requestImage(output[0][0],output[0][1]);
+            //requestImage(output[1][0],output[1][1]);
+
+            requestImage(output[0][0]);
+            requestImage(output[1][0]);
 
             // Pass the array's length to the navigation function
             navigation(last_index_value);
@@ -69,95 +73,98 @@
     // Needs enhancement of keypress
     function navigation(total) {
 
-        // Phase 2 enhancement is adding keypress support
+        // Debug
+        //console.log("The number of images in the gallery is " + total);
 
-        console.log("The number of images in the gallery is " + total);
+        jQuery("nav a").on("click",function (e) {
 
-        $("nav a").on("click", function (e) {
-
+            // Quell the default behavior
             e.preventDefault();
 
+            // Get a list of all elements
+            let elements = document.getElementsByTagName("figure");
 
-
-            // The element, in this case a figure
-            let element = jQuery("figure");
-
-            // LOAD IMAGES *************************** //
-            // Images load, in sequence, whether the 'next' or 'previous' buttons are clicked.
-
-            // Total of already loaded images. Default is 2.
-            let loaded_image_total = document.getElementsByTagName("img").length;
-
-            // Set values for the eventual image request by pulling the
-            // attributes from the elements initially added to the DOM.
-            // The loaded_image_total variable will always be one less than we need.
-            let preload_id = element.eq(loaded_image_total).attr("id");
-            let preload_alt = element.eq(loaded_image_total).children("figcaption").text();
-
-            // Unless all images are already loaded, request a single image
-            if (loaded_image_total !== total){
-                requestImage(preload_id, preload_alt);
-            }
-
-            let increment_value;
-
-            // The index of the currently active element
-            let active_element_index = $("figure.active").index();
-            //console.log("The index of the currently active element: " + active_element_index);
-
-            // Default to the action for the left (previous) navigation element
-            increment_value = -1;
-
-            // If the "next" link is clicked, change the increment value
-            if ($(this).attr("id") === "next") {
+            // Set the navigation increment
+            // -1 for "previous" navigation link
+            let increment_value = -1;
+            // +1 for the "next" navigation link
+            if (this.id  === "next") {
                 increment_value = 1;
-                //console.log("increment_value: " + increment_value)
             }
+
+            // Loop though the element list and determine:
+            // - the index of the currently active element
+            // - the total of already loaded images by counting style attributes (default is 2)
+            let unloaded_images = [], active_element_index = 0, i = -1;
+            for (let element of elements) {
+                i++;
+                if(1 === element.style.length) {
+                    unloaded_images.push(element.id);
+                }
+                if(element.classList[0] === "active") {
+                    active_element_index = i;
+                }
+            }
+
+            // console.log(elements[active_element_index].id);
+            // console.log(elements[active_element_index].previousSibling.id);
+
+            // SHOW ACTIVE SLIDE ********************* //
 
             // The next or previous element in the sequence to make active
-            let active_index = active_element_index + increment_value;
-            //console.log("active_index: " + active_index);
+            let next_active_element_index = active_element_index + increment_value;
 
-            // Clear classes to start over
-            element.removeClass("active");
-            element.addClass("inactive").hide();
+            // Debug
+            //console.log(`TRACE\nincrement_value: ${increment_value}\nunloaded_images: ${unloaded_images}\nactive_element_index: ${active_element_index}\nnext_active_element_index: ${next_active_element_index}`);
 
-            // Add the active class to display the image
-            if (active_index === total) {
-                // If the active index is at the end, go back and activate the first element
-                element.eq(0).removeClass("inactive");
-                element.eq(0).addClass("active").fadeIn();
-            } else {
-                element.eq(active_index).removeClass("inactive");
-                element.eq(active_index).addClass("active").fadeIn();
+            // If "next" is clicked on the last slide, show the first slide
+            if (active_element_index === (total - 1) && increment_value === 1) {
+                next_active_element_index = 0;
             }
 
-            // Show the 'previous' button when there are more than half of the total images loaded
-            // && active_element_index === 0
-            console.log("total / 2: " + total / 2);
-            console.log("loaded_image_total: " + loaded_image_total);
-            if(loaded_image_total > (total / 2)) {
-                document.querySelector("#prev").style.display = "block";
+            // If "previous" is clicked on the first slide, show the last slide
+            if(active_element_index === 0 && increment_value === -1 ) {
+                next_active_element_index = total - 1;
             }
 
-            // Transition from first slide to the rest of the slides.
-            // Obviously, this needs to be refined.
-            if($(".active").index() !== 0) {
-                $("h1").addClass("variation");
-            } else {
-                $("h1").removeClass("variation");
+            // Replace the class names
+            elements[active_element_index].className = elements[active_element_index].className.replace("active","inactive");
+            elements[next_active_element_index].className = elements[next_active_element_index].className.replace("inactive","active");
+
+
+            // Find the currently active image then
+            console.log(elements[active_element_index].previousSibling);
+
+
+            // Find the the previous element's ID then
+            // If that ID is in the unloaded image array
+            if (elements[active_element_index].previousSibling !== null) {
+                let previous_element_id = elements[active_element_index].previousSibling.id;
+                if(unloaded_images.lastIndexOf(previous_element_id) === -1) {
+                    console.log("The is a previous image.");
+                    document.getElementById("prev").style.display = "block";
+                } else {
+                    document.getElementById("prev").style.display = "none";
+                }
             }
 
+            // Show the previous button
+
+
+            // LOAD IMAGE **************************** //
+
+            //console.log(unloaded_images[0]);
+            // Load the first image in the unloaded images array
+            if(unloaded_images.length > 0) {
+                requestImage(unloaded_images[0]);
+            }
 
         });
     }
 
-    // Just returns the data from the initial request do that it can be used elsewhere.
-    //function handleData(){}
-
     // Process the data externally by passing in a callback function
     function requestGalleryMembers(handleData) {
-        $.ajax({
+        jQuery.ajax({
             url: url_allphotos,
             type: "GET",
             cache: true,
@@ -172,7 +179,7 @@
                 let photosArray = [];
 
                 // Iterate over the response and create a simplified array
-                $.each(data.photoset.photo, function (i, obj) {
+                jQuery.each(data.photoset.photo, function (i, obj) {
                     photosArray.push([obj.id, obj.title]);
                 });
 
@@ -188,13 +195,16 @@
     // Get the individual image from the getSizes method
     // id - the id of the image
     // altText - the alternative text of the image
-    function requestImage (id, altText) {
+    function requestImage (id) {
 
         // Debug
         //d = new Date();
         //console.log("requestImage called. " + d.getTime());
 
-        $.ajax({
+        // 6/2/2020
+        // Because we're not using the IMG tag, use the altText for the figure title attribute instead
+
+        jQuery.ajax({
             url: url_onephoto + id,
             type: "GET",
             cache: true,
@@ -205,15 +215,11 @@
                 // Instantiate the size array
                 let sizes_array = [];
 
-                // Create a new image object
-                let img = new Image();
-
                 // The index of the sizes array to get the path from
                 let size_index = 0;
 
                 // Push the available widths into the array for comparison
-                $.each(data.sizes.size, function (i, obj) {
-                    //console.log(obj.width);
+                jQuery.each(data.sizes.size, function (i, obj) {
                     sizes_array.push(obj.width);
                 });
 
@@ -224,20 +230,16 @@
                 );
 
                 // Get the index of the chosen resolution in order to choose the right image source.
-                $.each(data.sizes.size, function (i, obj) {
+                jQuery.each(data.sizes.size, function (i, obj) {
                     if (output === obj.width) {
                         size_index = i;
                     }
                 });
 
-                // Add attributes to the image object
-                $(img).attr({
-                    src: data.sizes.size[size_index].source,
-                    alt: altText
-                });
-
                 // Append the element in the proper ID location
-                $("figure#" + id).append(img);
+                jQuery("figure#" + id).css({
+                    'background-image':'url(' + data.sizes.size[size_index].source + ')'
+                });
 
             },
             error: function () {
@@ -249,15 +251,27 @@
     // Add the figures into the DOM, loaded with the IDs and captions from the initial API request
     // imageid = The individual photo ID
     // captiontext = The individual photo caption text
-    function addFigures (imageid, captiontext, x) {
-        let figcaption = $("<figcaption>").text(captiontext + " (" + x + ")");
-        let figure = $("<figure>", {
+    // position = the position in the sequence
+    function addFigures (imageid, captiontext, position) {
+
+        let figure_height = window.innerHeight - (document.getElementById('gallery').offsetTop - 1);
+
+        let figcaption = jQuery("<figcaption>").text(captiontext + " (" + position + ")");
+        let figure = jQuery("<figure>", {
                         id: imageid,
-                        class: "inactive"
+                        class: "inactive",
+                        title: captiontext,
+                        css: {"height":figure_height}
                         });
         figure.append(figcaption);
-        $("#gallery").append(figure);
+        jQuery("#gallery").append(figure);
     }
+
+
+    //const t = document.querySelector('figure').offsetTop;
+
+    //console.log(t);
+
 
     // Place arrows based on window size
     function placeArrows(h) {
@@ -265,15 +279,15 @@
         let arrowposition = Math.floor(((h - controloffset) / 2)) + "px";
         // Debug
         //console.log("controloffset=" + controloffset + ", arrowposition=" + arrowposition);
-        $("nav a").css("top", arrowposition);
+        jQuery("nav a").css("top", arrowposition);
     }
 
     // Display the gallery title
     function galleryTitle(title) {
         if (title) {
-            $("h1").text(title);
+            jQuery("h1").text(title);
         } else {
-            $("h1").text("Album Title Missing");
+            jQuery("h1").text("Album Title Missing");
         }
     }
 
