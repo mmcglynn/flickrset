39fa30ebf5d15b068aec5a73743f743a90cc1678
Michael McGlynn <mike@reignition.net>
2020-08-22 06:49:03 -0400

    Remove all jQuery references.

--- js/app.js
+++ js/app.js
@@ -24,11 +24,12 @@
 
     // API request URL for an individual photo
     // https://www.flickr.com/services/api/flickr.photos.getSizes.html
-    const url_onephoto = `${api}?method=flickr.photos.getSizes&api_key=${api_key}&format=json&jsoncallback=?&photo_id=`;
+    const url_onephoto = `${api}?method=flickr.photos.getSizes&api_key=${api_key}&format=json&nojsoncallback=1&photo_id=`;
 
     // API request URL for all photos in the gallery
     // https://www.flickr.com/services/api/flickr.photosets.getPhotos.html
-    const url_allphotos = `${api}?method=flickr.photosets.getPhotos&api_key=${api_key}&photoset_id=${photoset_id}&format=json&jsoncallback=?`;
+    //const url_allphotos = `${api}?method=flickr.photosets.getPhotos&api_key=${api_key}&photoset_id=${photoset_id}&format=json&jsoncallback=?`;
+    const url_allphotos = `${api}?method=flickr.photosets.getPhotos&api_key=${api_key}&photoset_id=${photoset_id}&format=json&nojsoncallback=1`;
 
     // API request URL for all photos in the gallery
     // https://www.flickr.com/services/api/flickr.photosets.getList.html
@@ -45,33 +46,26 @@
         });
     }
 
+    requestGalleryMembers(function (output) {
 
-    // Run requestGalleryMembers when DOM is ready
-    jQuery(document).ready(function () {
+        // The integer of the last index in the photos array
+        let last_index_value = output.length;
 
-        requestGalleryMembers(function (output) {
+        // Loop through the created gallery photo array
+        for (let [i, obj] of output.entries() ) {
+            addFigures(obj[0], obj[1], i + 1);
+        }
 
-            // The integer of the last index in the photos array
-            let last_index_value = output.length;
+        // Because all figures are initally set to inactive, set the first one to active
+        document.getElementsByTagName("figure")[0].classList.remove("inactive");
+        document.getElementsByTagName("figure")[0].classList.add("active");
 
-            // Loop through the created gallery photo array
-            jQuery.each(output, function (index, item) {
-                // Add the slide containers, which in this case are figures and figcaptions
-                addFigures(item[0], item[1], index + 1);
-            });
+        requestImage(output[0][0],output[0][1]);
+        requestImage(output[1][0],output[1][1]);
 
-            // Because all figures are initally set to inactive, set the first one to active
-            jQuery("figure:first").removeClass("inactive").addClass("active");
-
-            requestImage(output[0][0],output[0][1]);
-            requestImage(output[1][0],output[1][1]);
-
-            // Pass the array's length to the navigation function
-            navigation(last_index_value);
-        });
-
+        // Pass the array's length to the navigation function
+        navigation(last_index_value);
     });
-
 
     // Navigate through the slides, preloading images along the way
     // Needs enhancement of keypress
@@ -79,8 +73,6 @@
 
         // Get a list of all elements
         let elements = document.getElementsByTagName("figure");
-
-
 
         // Handle the arrow navigation links
         let navitems = document.querySelectorAll("nav a");
@@ -120,7 +112,6 @@
 
         // Need to handle when previous keyboard control is activated
         function advance_slides(increment) {
-
 
             // Count the images already loaded
             let loaded_images = document.getElementsByTagName("img").length;
@@ -162,6 +153,7 @@
                 console.log("active_element_index: " + active_element_index);
                 console.log("next_active_element_index: " + next_active_element_index);
                 console.log("total: " + total);
+                console.log("=============================");
 
                 // Going forward from the last slide, move to the first slide
                 if (next_active_element_index === total) {
@@ -184,193 +176,14 @@
             }
 
         }
-
-    //     // Navigate the slides and load the images using events
-    //     function advance_slides(direction) {
-    //         // Get a list of all elements
-    //         let elements = document.getElementsByTagName("figure");
-    //
-    //         let elements_length = total - 1;
-    //
-    //         //console.log("elements[elements_length].style.length: " + elements[elements_length].style.length);
-    //
-    //         // Set the navigation increment
-    //         // -1 for "previous" navigation link
-    //         let increment_value = -1;
-    //         // +1 for the "next" navigation link
-    //         if (direction  === "next") {
-    //             increment_value = 1;
-    //         }
-    //
-    //         // Loop though the element list and determine:
-    //         // - the index of the currently active element
-    //         // - the total of already loaded images by counting style attributes (default is 2)
-    //         let unloaded_images = [], active_element_index = 0, i = -1;
-    //         for (let element of elements) {
-    //             i++;
-    //             if( 1 === element.style.length ) {
-    //                 unloaded_images.push(element.id);
-    //             }
-    //             if( element.classList[0] === "active" ) {
-    //                 active_element_index = i;
-    //             }
-    //         }
-    //
-    //         // SHOW ACTIVE SLIDE ********************* //
-    //
-    //         // The next or previous element in the sequence to make active
-    //         let next_active_element_index = active_element_index + increment_value;
-    //
-    //         // Debug
-    //         //console.log(`TRACE\nincrement_value: ${increment_value}\nunloaded_images: ${unloaded_images}\nactive_element_index: ${active_element_index}\nnext_active_element_index: ${next_active_element_index}`);
-    //
-    //         // If the first image has a background image, show the previous button
-    //         if (elements[0].style.length > 1) {
-    //             console.log("1. The first image has a background image.");
-    //             document.getElementById("prev").style.display = "block";
-    //         }
-    //
-    //         // The the first slide is shown and the last slide has no image, hide the "prev" button.
-    //         if (active_element_index === 1 && elements[elements_length].style.length < 2) {
-    //             console.log("2. The first slide is shown and the last slide has no image.");
-    //             document.getElementById("prev").style.display = "none";
-    //         }
-    //
-    //         // If "next" is clicked on the last slide, show the first slide
-    //         if (active_element_index === elements_length && increment_value === 1) {
-    //             console.log("3. This is the first slide.");
-    //             next_active_element_index = 0;
-    //         }
-    //
-    //         // If "previous" is clicked on the first slide, show the last slide
-    //         if(active_element_index === 0 && increment_value === -1 ) {
-    //             console.log("4. This is the last slide.");
-    //             next_active_element_index = elements_length;
-    //         }
-    //
-    //         // Replace the class names
-    //         elements[active_element_index].className = elements[active_element_index].className.replace("active","inactive");
-    //         elements[next_active_element_index].className = elements[next_active_element_index].className.replace("inactive","active");
-    //
-    //         // LOAD IMAGE **************************** //
-    //         // Load the first image in the unloaded images array
-    //         if(unloaded_images.length > 0) {
-    //             requestImage(unloaded_images[0]);
-    //         }
-    //     }
     }
-
-
-    // // Navigate through the slides, preloading images along the way
-    // // Needs enhancement of keypress
-    // function navigation(total) {
-    //
-    //     jQuery("nav a").on("click",function (e) {
-    //         e.preventDefault();
-    //         advance_slides(this.id);
-    //     });
-    //
-    //     jQuery(document).keydown(function(e) {
-    //         let direction = "";
-    //         e.preventDefault();
-    //         // FIND THE PROPER METHOD
-    //         switch(e.which) {
-    //             case 37:
-    //                 direction = "prev";
-    //                 break;
-    //             case 39:
-    //                 direction = "next";
-    //                 break;
-    //             default: return;
-    //         }
-    //         advance_slides(direction);
-    //     });
-    //
-    //     // Navigate the slides and load the images using events
-    //     function advance_slides(direction) {
-    //         // Get a list of all elements
-    //         let elements = document.getElementsByTagName("figure");
-    //
-    //         let elements_length = total - 1;
-    //
-    //         //console.log("elements[elements_length].style.length: " + elements[elements_length].style.length);
-    //
-    //         // Set the navigation increment
-    //         // -1 for "previous" navigation link
-    //         let increment_value = -1;
-    //         // +1 for the "next" navigation link
-    //         if (direction  === "next") {
-    //             increment_value = 1;
-    //         }
-    //
-    //         // Loop though the element list and determine:
-    //         // - the index of the currently active element
-    //         // - the total of already loaded images by counting style attributes (default is 2)
-    //         let unloaded_images = [], active_element_index = 0, i = -1;
-    //         for (let element of elements) {
-    //             i++;
-    //             if( 1 === element.style.length ) {
-    //                 unloaded_images.push(element.id);
-    //             }
-    //             if( element.classList[0] === "active" ) {
-    //                 active_element_index = i;
-    //             }
-    //         }
-    //
-    //         // SHOW ACTIVE SLIDE ********************* //
-    //
-    //         // The next or previous element in the sequence to make active
-    //         let next_active_element_index = active_element_index + increment_value;
-    //
-    //         // Debug
-    //         //console.log(`TRACE\nincrement_value: ${increment_value}\nunloaded_images: ${unloaded_images}\nactive_element_index: ${active_element_index}\nnext_active_element_index: ${next_active_element_index}`);
-    //
-    //         // If the first image has a background image, show the previous button
-    //         if (elements[0].style.length > 1) {
-    //             console.log("1. The first image has a background image.");
-    //             document.getElementById("prev").style.display = "block";
-    //         }
-    //
-    //         // The the first slide is shown and the last slide has no image, hide the "prev" button.
-    //         if (active_element_index === 1 && elements[elements_length].style.length < 2) {
-    //             console.log("2. The first slide is shown and the last slide has no image.");
-    //             document.getElementById("prev").style.display = "none";
-    //         }
-    //
-    //         // If "next" is clicked on the last slide, show the first slide
-    //         if (active_element_index === elements_length && increment_value === 1) {
-    //             console.log("3. This is the first slide.");
-    //             next_active_element_index = 0;
-    //         }
-    //
-    //         // If "previous" is clicked on the first slide, show the last slide
-    //         if(active_element_index === 0 && increment_value === -1 ) {
-    //             console.log("4. This is the last slide.");
-    //             next_active_element_index = elements_length;
-    //         }
-    //
-    //         // Replace the class names
-    //         elements[active_element_index].className = elements[active_element_index].className.replace("active","inactive");
-    //         elements[next_active_element_index].className = elements[next_active_element_index].className.replace("inactive","active");
-    //
-    //         // LOAD IMAGE **************************** //
-    //         // Load the first image in the unloaded images array
-    //         if(unloaded_images.length > 0) {
-    //             requestImage(unloaded_images[0]);
-    //         }
-    //     }
-    // }
 
     // Process the data externally by passing in a callback function
     function requestGalleryMembers(handleData) {
-        jQuery.ajax({
-            url: url_allphotos,
-            type: "GET",
-            cache: true,
-            dataType: "jsonp",
-            async: false,
-            success: function (data) {
 
+        fetch(url_allphotos)
+            .then(response => response.json())
+            .then(function (data) {
                 // Display the gallery title
                 galleryTitle(data.photoset.title);
 
@@ -378,39 +191,28 @@
                 let photosArray = [];
 
                 // Iterate over the response and create a simplified array
-                jQuery.each(data.photoset.photo, function (i, obj) {
+                for (let obj of data.photoset.photo) {
                     photosArray.push([obj.id, obj.title]);
-                });
+                }
 
                 // Pass the new array to the callback function;
                 handleData(photosArray);
-            },
-            error: function () {
-                console.log("Gallery request has failed.");
-            }
-        });
+            })
+            .catch(error => {
+                console.warn(error);
+            });
+
     }
+
 
     // Get the individual image from the getSizes method
     // id - the id of the image
     // altText - the alternative text of the image
     function requestImage(id, altText) {
 
-        // Debug
-        //d = new Date();
-        //console.log("requestImage called. " + d.getTime());
-
-        // 6/2/2020
-        // Because we're not using the IMG tag, use the altText for the figure title attribute instead
-
-        jQuery.ajax({
-            url: url_onephoto + id,
-            type: "GET",
-            cache: true,
-            dataType: "jsonp", // why?
-            async: false,
-            success: function (data) {
-
+        fetch(url_onephoto + id)
+            .then(response => response.json())
+            .then(function (data) {
                 // Instantiate the size array
                 let sizes_array = [];
 
@@ -421,9 +223,9 @@
                 let size_index = 0;
 
                 // Push the available widths into the array for comparison
-                jQuery.each(data.sizes.size, function (i, obj) {
+                for (let obj of data.sizes.size) {
                     sizes_array.push(obj.width);
-                });
+                }
 
                 // Find the closest value in the array to the viewport's width.
                 // This will determine which image size we request.
@@ -432,41 +234,25 @@
                 );
 
                 // Get the index of the chosen resolution in order to choose the right image source.
-                jQuery.each(data.sizes.size, function (i, obj) {
+                for (let [i, obj] of data.sizes.size.entries() ) {
                     if (output === obj.width) {
                         size_index = i;
                     }
-                });
-
-                // Experimental for replacing the loop above with ES6
-                // 7/23/2020
-                // for (let obj of data.sizes.size ) {
-                //     if (output === obj.width) {
-                //         size_index = obj.index;
-                //     }
-                // }
+                }
 
                 // Add attributes to the image object
-                $(img).attr({
-                    src: data.sizes.size[size_index].source,
-                    alt: altText,
-                    title: altText
-                });
+                img.src = data.sizes.size[size_index].source;
+                img.alt = altText;
+                img.title = altText;
 
                 // Append the element in the proper ID location
-                $("figure#" + id).append(img);
-
-                // Append the element in the proper ID location
-                // jQuery("figure#" + id).css({
-                //     'background-image': 'url(' + data.sizes.size[size_index].source + ')'
-                // });
-
-            },
-            error: function () {
-                console.log("Data was not retrieved.");
-            }
-        });
+                document.getElementById(id).append(img);
+            })
+            .catch(error => {
+                console.warn(error);
+            });
     }
+
 
     // Add the figures into the DOM, loaded with the IDs and captions from the initial API request
     // imageid = The individual photo ID
@@ -476,33 +262,37 @@
 
         let figure_height = window.innerHeight - (document.getElementById('gallery').offsetTop);
 
-        let figcaption = jQuery("<figcaption>").text(captiontext + " (" + position + ")");
-        let figure = jQuery("<figure>", {
-            id: imageid,
-            class: "inactive",
-            title: captiontext,
-            css: {"height": figure_height}
-        });
+        // Build the figcaption element and contents
+        let figcaption = document.createElement("figcaption");
+        figcaption.append(document.createTextNode(captiontext + " (" + position + ")"));
+
+        // Build the figure element
+        let figure = document.createElement("figure");
+        figure.classList.add("inactive");
+        figure.id = imageid;
+        figure.title = captiontext;
+        figure.style.height = figure_height + "px";
+
+        // Add the new elements to the DOM
         figure.append(figcaption);
-        jQuery("#gallery").append(figure);
+        document.getElementById("gallery").append(figure);
     }
 
     // Place arrows based on window size
     function placeArrows(h) {
         let controloffset = document.getElementById("next").offsetHeight;
         let arrowposition = Math.floor(((h - controloffset) / 2)) + "px";
-        // Debug
-        //console.log("controloffset=" + controloffset + ", arrowposition=" + arrowposition);
-        jQuery("nav").css("top", arrowposition);
+        let nav = document.getElementsByTagName("nav");
+        nav[0].style.top = arrowposition;
     }
 
     // Display the gallery title
     function galleryTitle(title) {
-        if (title) {
-            jQuery("h1").text(title);
-        } else {
-            jQuery("h1").text("Album Title Missing");
+        let h1 = document.getElementsByTagName("h1");
+        if (!title) {
+            title = "Album Title Missing";
         }
+        h1[0].innerText = title;
     }
 
     // Locate the ID in the URL
@@ -521,11 +311,9 @@
             if (params.has("id")) {
                 // Get the id's value
                 id = url.searchParams.get("id");
-                //console.log(photoset_id);
             }
         } else {
-            // Need to have a better default.
-            id = "72157657718008461";
+            alert("There is no image available.");
         }
         return id;
     }
